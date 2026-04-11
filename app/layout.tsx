d7fc8a1c781977d2813e04e5ae8b06490dc0b25b
John Loyd Uy <johnloyduy5@gmail.com>
2026-04-12 03:27:56 +0800

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,15 @@
 import type { ReactNode } from "react";
+import type { Metadata } from "next";
+
+export const metadata = {
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon.png" },
+    ],
+    apple: "/favicon.png",
+  },
+}
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
